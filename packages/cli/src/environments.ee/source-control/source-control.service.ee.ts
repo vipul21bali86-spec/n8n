73e7e052e59af3891657a2470e78a7f795f502ce
Guillaume Jacquart <jacquart.guillaume@gmail.com>
2025-08-23 08:30:15 +0200

    Extract resource handler

--- packages/cli/src/environments.ee/source-control/source-control.service.ee.ts
+++ packages/cli/src/environments.ee/source-control/source-control.service.ee.ts
@@ -38,6 +38,7 @@ import {
 	normalizeAndValidateSourceControlledFilePath,
 	sourceControlFoldersExistCheck,
 } from './source-control-helper.ee';
+import { GenericResourceHandler } from './resource-handlers/generic-resource-handler';
 import { SourceControlImportService } from './source-control-import.service.ee';
 import { SourceControlPreferencesService } from './source-control-preferences.service.ee';
 import { SourceControlScopedService } from './source-control-scoped.service';
@@ -72,6 +73,7 @@ export class SourceControlService {
 		private tagRepository: TagRepository,
 		private folderRepository: FolderRepository,
 		private readonly eventService: EventService,
+		private readonly resourceHandler: GenericResourceHandler,
 	) {
 		const { gitFolder, sshFolder, sshKeyName } = sourceControlPreferencesService;
 		this.gitFolder = gitFolder;
@@ -307,13 +309,15 @@ export class SourceControlService {
 
 		this.sourceControlExportService.rmFilesFromExportFolder(filesToBeDeleted);
 
-		const workflowsToBeExported = filesToPush.filter(
-			(e) => e.type === 'workflow' && e.status !== 'deleted',
+		const workflowsToBeExported = this.resourceHandler.getNonDeletedResources(
+			filesToPush,
+			'workflow',
 		);
 		await this.sourceControlExportService.exportWorkflowsToWorkFolder(workflowsToBeExported);
 
-		const credentialsToBeExported = filesToPush.filter(
-			(e) => e.type === 'credential' && e.status !== 'deleted',
+		const credentialsToBeExported = this.resourceHandler.getNonDeletedResources(
+			filesToPush,
+			'credential',
 		);
 		const credentialExportResult =
 			await this.sourceControlExportService.exportCredentialsToWorkFolder(credentialsToBeExported);
@@ -326,19 +330,19 @@ export class SourceControlService {
 			});
 		}
 
-		const tagChanges = filesToPush.find((e) => e.type === 'tags');
+		const tagChanges = this.resourceHandler.filterByType(filesToPush, 'tags')[0];
 		if (tagChanges) {
 			filesToBePushed.add(tagChanges.file);
 			await this.sourceControlExportService.exportTagsToWorkFolder(context);
 		}
 
-		const folderChanges = filesToPush.find((e) => e.type === 'folders');
+		const folderChanges = this.resourceHandler.filterByType(filesToPush, 'folders')[0];
 		if (folderChanges) {
 			filesToBePushed.add(folderChanges.file);
 			await this.sourceControlExportService.exportFoldersToWorkFolder(context);
 		}
 
-		const variablesChanges = filesToPush.find((e) => e.type === 'variables');
+		const variablesChanges = this.resourceHandler.filterByType(filesToPush, 'variables')[0];
 		if (variablesChanges) {
 			filesToBePushed.add(variablesChanges.file);
 			await this.sourceControlExportService.exportVariablesToWorkFolder();
@@ -374,46 +378,6 @@ export class SourceControlService {
 		return files.filter((file) => file.conflict || file.status === 'modified');
 	}
 
-	private getWorkflowsToImport(files: SourceControlledFile[]): SourceControlledFile[] {
-		return files.filter((e) => e.type === 'workflow' && e.status !== 'deleted');
-	}
-
-	private getWorkflowsToDelete(files: SourceControlledFile[]): SourceControlledFile[] {
-		return files.filter((e) => e.type === 'workflow' && e.status === 'deleted');
-	}
-
-	private getCredentialsToImport(files: SourceControlledFile[]): SourceControlledFile[] {
-		return files.filter((e) => e.type === 'credential' && e.status !== 'deleted');
-	}
-
-	private getCredentialsToDelete(files: SourceControlledFile[]): SourceControlledFile[] {
-		return files.filter((e) => e.type === 'credential' && e.status === 'deleted');
-	}
-
-	private getTagsToImport(files: SourceControlledFile[]): SourceControlledFile | undefined {
-		return files.find((e) => e.type === 'tags' && e.status !== 'deleted');
-	}
-
-	private getTagsToDelete(files: SourceControlledFile[]): SourceControlledFile[] {
-		return files.filter((e) => e.type === 'tags' && e.status === 'deleted');
-	}
-
-	private getVariablesToImport(files: SourceControlledFile[]): SourceControlledFile | undefined {
-		return files.find((e) => e.type === 'variables' && e.status !== 'deleted');
-	}
-
-	private getFoldersToImport(files: SourceControlledFile[]): SourceControlledFile | undefined {
-		return files.find((e) => e.type === 'folders' && e.status !== 'deleted');
-	}
-
-	private getFoldersToDelete(files: SourceControlledFile[]): SourceControlledFile[] {
-		return files.filter((e) => e.type === 'folders' && e.status === 'deleted');
-	}
-
-	private getVariablesToDelete(files: SourceControlledFile[]): SourceControlledFile[] {
-		return files.filter((e) => e.type === 'variables' && e.status === 'deleted');
-	}
-
 	async pullWorkfolder(
 		user: User,
 		options: PullWorkFolderRequestDto,
@@ -438,47 +402,65 @@ export class SourceControlService {
 		}
 
 		// Make sure the folders get processed first as the workflows depend on them
-		const foldersToBeImported = this.getFoldersToImport(statusResult);
+		const foldersToBeImported = this.resourceHandler.getNonDeletedResources(
+			statusResult,
+			'folders',
+		)[0];
 		if (foldersToBeImported) {
 			await this.sourceControlImportService.importFoldersFromWorkFolder(user, foldersToBeImported);
 		}
 
-		const workflowsToBeImported = this.getWorkflowsToImport(statusResult);
+		const workflowsToBeImported = this.resourceHandler.getNonDeletedResources(
+			statusResult,
+			'workflow',
+		);
 		await this.sourceControlImportService.importWorkflowFromWorkFolder(
 			workflowsToBeImported,
 			user.id,
 		);
-		const workflowsToBeDeleted = this.getWorkflowsToDelete(statusResult);
+		const workflowsToBeDeleted = this.resourceHandler.getDeletedResources(statusResult, 'workflow');
 		await this.sourceControlImportService.deleteWorkflowsNotInWorkfolder(
 			user,
 			workflowsToBeDeleted,
 		);
-		const credentialsToBeImported = this.getCredentialsToImport(statusResult);
+		const credentialsToBeImported = this.resourceHandler.getNonDeletedResources(
+			statusResult,
+			'credential',
+		);
 		await this.sourceControlImportService.importCredentialsFromWorkFolder(
 			credentialsToBeImported,
 			user.id,
 		);
-		const credentialsToBeDeleted = this.getCredentialsToDelete(statusResult);
+		const credentialsToBeDeleted = this.resourceHandler.getDeletedResources(
+			statusResult,
+			'credential',
+		);
 		await this.sourceControlImportService.deleteCredentialsNotInWorkfolder(
 			user,
 			credentialsToBeDeleted,
 		);
 
-		const tagsToBeImported = this.getTagsToImport(statusResult);
+		const tagsToBeImported = this.resourceHandler.getNonDeletedResources(statusResult, 'tags')[0];
 		if (tagsToBeImported) {
 			await this.sourceControlImportService.importTagsFromWorkFolder(tagsToBeImported);
 		}
-		const tagsToBeDeleted = this.getTagsToDelete(statusResult);
+		const tagsToBeDeleted = this.resourceHandler.getDeletedResources(statusResult, 'tags');
 		await this.sourceControlImportService.deleteTagsNotInWorkfolder(tagsToBeDeleted);
 
-		const variablesToBeImported = this.getVariablesToImport(statusResult);
+		const variablesToBeImported = this.resourceHandler.getNonDeletedResources(
+			statusResult,
+			'variables',
+		)[0];
 		if (variablesToBeImported) {
 			await this.sourceControlImportService.importVariablesFromWorkFolder(variablesToBeImported);
 		}
-		const variablesToBeDeleted = this.getVariablesToDelete(statusResult);
+		const variablesToBeDeleted = this.resourceHandler.getDeletedResources(
+			statusResult,
+			'variables',
+		);
 		await this.sourceControlImportService.deleteVariablesNotInWorkfolder(variablesToBeDeleted);
 
-		const foldersToBeDeleted = this.getFoldersToDelete(statusResult);
+		const foldersToBeDeleted = this.resourceHandler.getDeletedResources(statusResult, 'folders');
 		await this.sourceControlImportService.deleteFoldersNotInWorkfolder(foldersToBeDeleted);
 
 		// #region Tracking Information
